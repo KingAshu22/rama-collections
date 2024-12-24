c76fd40aa7c5f1851c477b80a6594a4c61afe3f3
KingAshu22 <ashishprasad9833@gmail.com>
2024-12-24 09:26:47 +0530

    deleted create-lehenga

--- app/(route)/create-lehenga/page.js
+++
@@ -1,9 +0,0 @@
-import LehengaForm from "./_components/LehengaForm";
-
-export default function Home() {
-    return (
-        <div className="min-h-screen flex items-center justify-center">
-            <LehengaForm />
-        </div>
-    );
-}
